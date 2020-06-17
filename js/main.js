f789bc73960cb71d64ee6db767701f27c8a9f4c7
EdinilsonProgramador <menendes.edinilson@gmail.com>
2020-06-16 22:19:59 -0300

    Inicio do JS

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function clicou(){
+    alert("Obrigado por Clicar");
+}
+
+
+
+
+/*var idade = prompt("Qual a sua idade??");
+if (idade >= 18){
+    alert('Maior de Idade!!!');
+}else{
+    alert("Menor de Idade!!!");
+};
+
+//var nome = "Edinilson Menendes";
+//alert("Welcome to JS"  + nome);
